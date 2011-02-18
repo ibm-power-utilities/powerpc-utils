e56f3fcfb1763868bb65812b3e170341f181ec2f
Nathan Fontenot <nfont@austin.ibm.com>
2011-02-18 09:37:39 -0600

    Initial release of the lparstat command.
    Signed-off-by: Nathan Fontenot <nfont@austin.ibm.com>

---
+++ src/lparstat.c
@@ -0,0 +1,489 @@
+/**
+ * @file lparstat.c
+ * @brief lparstat command
+ *
+ * Copyright (c) 2011 International Business Machines
+ * Common Public License Version 1.0 (see COPYRIGHT)
+ *
+ * @author Nathan Fontenot <nfont@austin.ibm.com>
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include "lparstat.h"
+
+#define LPARCFG_FILE	"/proc/ppc64/lparcfg"
+#define SE_NOT_FOUND	"???"
+#define SE_NOT_VALID	"-"
+
+struct sysentry *get_sysentry(char *name)
+{
+	struct sysentry *se = &system_data[0];
+
+	while (se->name[0] != '\0') {
+		if (!strcmp(se->name, name))
+			return se;
+		se++;
+	}
+
+	return NULL;
+}
+
+void get_sysdata(char *name, char **descr, char *value)
+{
+	struct sysentry *se;
+
+	se = get_sysentry(name);
+	if (!se) {
+		*descr = name;
+		sprintf(value, SE_NOT_FOUND);
+		return;
+	}
+
+	if (se->get) {
+		se->get(se, value);
+	} else if (se->value[0] == '\0') {
+		sprintf(value, SE_NOT_VALID);
+	} else {
+		sprintf(value, se->value);
+	}
+
+	*descr = se->descr;
+}
+
+void get_time()
+{
+	struct timeval t;
+	struct sysentry *se;
+
+	gettimeofday(&t, 0);
+
+	se = get_sysentry("time");
+	sprintf(se->value, "%ld", t.tv_sec + t.tv_usec);
+}
+
+long long elapsed_time()
+{
+	long long newtime, oldtime = 0;
+	struct sysentry *se;
+
+	se = get_sysentry("time");
+	newtime = strtoll(se->value, NULL, 0);
+	oldtime = strtoll(se->old_value, NULL, 0);
+
+	return newtime - oldtime;
+}
+
+int get_time_base()
+{
+	FILE *f;
+	char buf[80];
+	char *tb = NULL;
+	struct sysentry *se;
+
+	f = fopen("/proc/cpuinfo", "r");
+	while ((fgets(buf, 80, f)) != NULL) {
+		if (!strncmp(buf, "timebase", 8)) {
+			tb = strchr(buf, ':') + 2;
+			break;
+		}
+	}
+	fclose(f);
+
+	if (!tb)
+		return -1;
+
+	se = get_sysentry("timebase");
+	sprintf(se->value, tb);
+	return 0;
+}
+
+void get_cpu_physc(struct sysentry *unused_se, char *buf)
+{
+	struct sysentry *se;
+	float elapsed;
+	float new_purr, old_purr;
+	float timebase, physc;
+
+	elapsed = elapsed_time();
+
+	se = get_sysentry("timebase");
+	timebase = atoi(se->value);
+
+	se = get_sysentry("purr");
+	new_purr = strtoll(se->value, NULL, 0);
+	old_purr = strtoll(se->old_value, NULL, 0);
+
+	physc = (new_purr - old_purr)/timebase/elapsed;
+	sprintf(buf, "%.2f", physc);
+}
+
+int parse_lparcfg()
+{
+	FILE *f;
+	char line[128];
+
+	f = fopen(LPARCFG_FILE, "r");
+	if (!f) {
+		fprintf(stderr, "Could not open %s\n", LPARCFG_FILE);
+		return -1;
+	}
+
+	/* parse the file skipping the first line */
+	fgets(line, 128, f);
+	while (fgets(line, 128, f) != NULL) {
+		char *name, *value, *nl;
+		struct sysentry *se;
+
+		if (line[0] == '\n')
+			continue;
+
+		name = &line[0];
+		value = strchr(line, '=');
+		*value = '\0';
+		value++;
+
+		nl = strchr(value, '\n');
+		*nl = '\0';
+		
+		se = get_sysentry(name);
+		if (se)
+			strncpy(se->value, value, SYSDATA_VALUE_SZ);
+	}
+
+	fclose(f);
+	return 0;
+}
+
+int parse_proc_stat()
+{
+	FILE *f;
+	char line[128];
+	char *value;
+	int i, entries = 6;
+	long long statvals[entries];
+	struct sysentry *se;
+	char *names[] = {"cpu_total", "cpu_user", "cpu_nice", "cpu_sys",
+			 "cpu_idle", "cpu_iowait"};
+
+	/* we just need the first line */
+	f = fopen("/proc/stat", "r");
+	fgets(line, 128, f);
+	fclose(f);
+
+	statvals[0] = 0;
+	value = line;
+	for (i = 1; i <= entries; i++) {
+		int v;
+		value = strchr(value, ' ') + 1;
+		if (i == 1)
+			value++;
+		v = atoi(value);
+		statvals[i] = v;
+		statvals[0] += v;
+	}
+
+	for (i = 0; i < entries; i++) {
+		se = get_sysentry(names[i]);
+		sprintf(se->value, "%lld", statvals[i]);
+	}
+
+	se = get_sysentry("cpu_lbusy");
+	sprintf(se->value, "%lld", statvals[1] + statvals[3]);
+
+	return 0;
+}
+
+void get_smt_state(struct sysentry *se, char *buf)
+{
+	char *value = "?";
+
+	if (se->value[0] == '1')
+		value = "Shared";
+	else 
+		value = "Dedicated";
+
+	sprintf(buf, value);
+}
+		
+void get_capped_mode(struct sysentry *se, char *buf)
+{
+	char *value = "?";
+
+	if (se->value[0] == '1')
+		value = "Capped";
+	else 
+		value = "Uncapped";
+
+	sprintf(buf, value);
+}
+
+void get_percent_entry(struct sysentry *se, char *buf)
+{
+	int value, p, r;
+
+	value = atoi(se->value);
+	p = value / 100;
+	r = value % 100;
+
+	sprintf(buf, "%d.%d", p, r);
+}
+
+void get_phys_cpu_percentage(struct sysentry *se, char *buf)
+{
+	struct sysentry *tmp_se;
+	int entcap, active;
+
+	tmp_se = get_sysentry("DesEntCap");
+	entcap = atoi(tmp_se->value);
+
+	tmp_se = get_sysentry("partition_active_processors");
+	active = atoi(tmp_se->value);
+
+	sprintf(buf, "%d", entcap/active);
+}
+
+void get_active_cpus_in_pool(struct sysentry *se, char *buf)
+{
+	struct sysentry *tmp;
+
+	tmp = get_sysentry("pool_capacity");
+	sprintf(buf, "%d", atoi(tmp->value)/100);
+}
+
+void get_memory_mode(struct sysentry *se, char *buf)
+{
+	struct sysentry *tmp;
+
+	tmp = get_sysentry("entitled_memory");
+	if (tmp->value[0] == '\0')
+		sprintf(buf, "Dedicated");
+	else
+		sprintf(buf, "Shared");
+}
+
+void get_name(const char *file, char *buf)
+{
+	FILE *f;
+	char tmpbuf[64];
+
+	f = fopen(file, "r");
+	fread(tmpbuf, 64, 1, f);
+	fclose(f);
+
+	sprintf(buf, "%s", tmpbuf);
+}
+
+void get_node_name(struct sysentry *se, char *buf)
+{
+	char *nl;
+
+	get_name("/proc/sys/kernel/hostname", buf);
+
+	/* For some reason this doesn't get null-terminated and makes
+	 * for ugly output.
+	 */
+	nl = strchr(buf, '\n');
+	*nl = '\0';
+}
+
+void get_partition_name(struct sysentry *se, char *buf)
+{
+	return get_name("/proc/device-tree/ibm,partition-name", buf);
+}
+
+void get_mem_total(struct sysentry *se, char *buf)
+{
+	FILE *f;
+	char line[128];
+	char *mem, *nl;
+
+	f = fopen("/proc/meminfo", "r");
+	fgets(line, 128, f);
+	fclose(f);
+
+	mem = strchr(line, ':');
+	do {
+		mem++;
+	} while (*mem == ' ');
+
+	nl = strchr(mem, '\n');
+	*nl = '\0';
+
+	sprintf(buf, mem);
+}
+
+void get_smt_mode(struct sysentry *se, char *buf)
+{
+	FILE *f;
+	char line[128];
+	char *cmd = "/usr/sbin/ppc64_cpu --smt";
+
+	f = popen(cmd, "r");
+	fgets(line, 128, f);
+	pclose(f);
+
+	/* The output is either "SMT is on" or "SMT is off", we can cheat
+	 * by looking at line[8] for either 'n' or 'f'.
+	 */
+	if (line[8] == 'n')
+		sprintf(buf, "On");
+	else if (line[8] == 'f')
+		sprintf(buf, "Off");
+	else
+		sprintf(buf, "Unknown");
+}
+
+long long get_cpu_time_diff()
+{
+	long long old_total = 0, new_total = 0;
+	struct sysentry *se;
+
+	se = get_sysentry("cpu_total");
+	new_total = strtoll(se->value, NULL, 0);
+	old_total = strtoll(se->old_value, NULL, 0);
+
+	return new_total - old_total;
+}
+
+void get_cpu_stat(struct sysentry *se, char *buf)
+{
+	float total, percent;
+	float old_val, new_val;
+
+	total = get_cpu_time_diff();
+	new_val = atoi(se->value);
+	old_val = atoi(se->old_value);
+	percent = (float)((new_val - old_val)/total) * 100;
+	sprintf(buf, "%.2f", percent);
+}
+
+void init_sysdata(void)
+{
+	get_time();
+	parse_lparcfg();
+	parse_proc_stat();
+	get_time_base();
+}
+
+void update_sysdata(void)
+{
+	struct sysentry *se = &system_data[0];
+	while (se->name[0] != '\0') {
+		memcpy(se->old_value, se->value, SYSDATA_VALUE_SZ);
+		se++;
+	}
+	
+	init_sysdata();
+}
+
+int print_iflag_data()
+{
+	char *fmt = "%-45s: %s\n";
+	char value[64];
+	char *descr;
+	int i = 0;
+
+	while (iflag_entries[i] != NULL) {
+		get_sysdata(iflag_entries[i], &descr, value);
+#ifndef DEBUG
+		if (strcmp(value, SE_NOT_VALID) && strcmp(value, SE_NOT_FOUND))
+#endif
+			fprintf(stdout, fmt, descr, value);
+		i++;
+	}
+
+	return 0;
+}
+
+void print_default_output(int interval, int count)
+{
+	char *fmt = "%5s %5s %5s %5s %5s %5s %5s %5s %5s\n";
+	char *descr;
+	char buf[128];
+	int offset;
+	char value[32];
+	char user[32], sys[32], wait[32], idle[32], physc[32], entc[32];
+	char lbusy[32], vcsw[32], phint[32];
+
+	memset(buf, 128, 0);
+	get_sysdata("shared_processor_mode", &descr, value);
+	offset = sprintf(buf, "type=%s ", value);
+	get_sysdata("capped", &descr, value);
+	offset += sprintf(buf + offset, "mode=%s ", value);
+	get_sysdata("smt_state", &descr, value);
+	offset += sprintf(buf + offset, "smt=%s ", value);
+	get_sysdata("system_active_processors", &descr, value);
+	offset += sprintf(buf + offset, "lcpu=%s ", value);
+	get_sysdata("MemTotal", &descr, value);
+	offset += sprintf(buf + offset, "mem=%s ", value);
+	get_sysdata("active_cpus_in_pool", &descr, value);
+	offset += sprintf(buf + offset, "cpus=%s ", value);
+	get_sysdata("DesEntCap", &descr, value);
+	offset += sprintf(buf + offset, "ent=%s ", value);
+
+	fprintf(stdout, "\nSystem Configuration\n%s\n\n", buf);
+
+	fprintf(stdout, fmt, "\%user", "\%sys", "\%wait", "\%idle", "physc",
+		"\%entc", "lbusy", "vcsw", "phint");
+	fprintf(stdout, fmt, "-----", "-----", "-----", "-----", "-----",
+		"-----", "-----", "-----", "-----");
+
+	do {
+		if (interval) {
+			sleep(interval);
+			update_sysdata();
+		}
+
+		get_sysdata("cpu_user", &descr, user);
+		get_sysdata("cpu_sys", &descr, sys);
+		get_sysdata("cpu_iowait", &descr, wait);
+		get_sysdata("cpu_idle", &descr, idle);
+		get_sysdata("cpu_lbusy", &descr, lbusy);
+		get_sysdata("dispatches", &descr, vcsw);
+		get_sysdata("physc", &descr, physc);
+
+		fprintf(stdout, fmt, user, sys, wait, idle, physc, "XXX",
+			lbusy, vcsw, "XXX");
+	} while (--count > 0);
+}
+
+int main(int argc, char *argv[])
+{
+	int interval = 0, count = 0;
+	int c;
+	int i_option = 0;
+
+	while ((c = getopt(argc, argv, "i")) != -1) {
+		switch(c) {
+			case 'i':
+				i_option = 1;
+				break;
+			case '?':
+			default:
+				break;
+		}
+	}
+
+	/* see if there is an interval specified */
+	if (optind < argc)
+		interval = atoi(argv[optind++]);
+
+	/* check for count specified */
+	if (optind < argc)
+		count = atoi(argv[optind++]);
+
+	init_sysdata();
+
+	if (i_option)
+		print_iflag_data();
+	else
+		print_default_output(interval, count);
+
+	return 0;
+}
---
+++ src/lparstat.h
@@ -0,0 +1,264 @@
+/**
+ * @file lparstat.h
+ * @brief lparstat command header
+ *
+ * Copyright (c) 2011 International Business Machines
+ * Common Public License Version 1.0 (see COPYRIGHT)
+ *
+ * @author Nathan Fontenot <nfont@austin.ibm.com>
+ */
+
+#define SYSDATA_VALUE_SZ	64
+#define SYSDATA_NAME_SZ		64
+#define SYSDATA_DESCR_SZ	128
+
+struct sysentry {
+	char	value[SYSDATA_VALUE_SZ];	/* value from file */
+	char	old_value[SYSDATA_VALUE_SZ];	/* previous value from file */
+	char	name[SYSDATA_NAME_SZ];		/* internal name */
+	char	descr[SYSDATA_DESCR_SZ];	/* description of data */
+	void (*get)(struct sysentry *, char *);
+};
+
+extern void get_smt_state(struct sysentry *, char *);
+extern void get_capped_mode(struct sysentry *, char *);
+extern void get_memory_mode(struct sysentry *, char *);
+extern void get_percent_entry(struct sysentry *, char *);
+extern void get_phys_cpu_percentage(struct sysentry *, char *);
+extern void get_active_cpus_in_pool(struct sysentry *, char *);
+extern void get_partition_name(struct sysentry *, char *);
+extern void get_node_name(struct sysentry *, char *);
+extern void get_mem_total(struct sysentry *, char *);
+extern void get_smt_mode(struct sysentry *, char *);
+extern void get_cpu_stat(struct sysentry *, char *);
+extern void get_cpu_physc(struct sysentry *, char *);
+
+struct sysentry system_data[] = {
+	/* System Names */
+	{.name = "node_name",
+	 .descr = "Node Name",
+	 .get = &get_node_name},
+	{.name = "partition_name",
+	 .descr = "Partition Name",
+	 .get = &get_partition_name},
+
+	/* lparcfg data */
+	{.name = "serial_number",
+	 .descr = "Serial Number"},
+	{.name = "system_type",
+	 .descr = "System Model"},
+	{.name = "partition_id",
+	 .descr = "Partition Number"},
+	{.name = "group",
+	 .descr = "Partition Group-ID"},
+	{.name = "BoundThrds",
+	 .descr = "Bound Threads"},
+	{.name = "CapInc",
+	 .descr = "Capacity Increment",
+	 .get = &get_percent_entry},
+	{.name = "DisWheRotPer",
+	 .descr = "Dispatch Wheel Rotation Period"},
+	{.name = "MinEntCap",
+	 .descr = "Minimum Capacity",
+	 .get = &get_percent_entry},
+	{.name = "MinEntCapPerVP",
+	 .descr = "Minimum Entitled Capacity per Virtual Processor"},
+	{.name = "MinProcs",
+	 .descr = "Minimum Virtual CPUs"},
+	{.name = "partition_max_entitled_capacity",
+	 .descr = "Maxiumum Capacity",
+	 .get = &get_percent_entry},
+	{.name = "system_potential_processors",
+	 .descr = "Maximum System Processors"},
+	{.name = "DesEntCap",
+	 .descr = "Entitled Capacity",
+	 .get = &get_percent_entry},
+	{.name = "DesProcs",
+	 .descr = "Desired Processors"},
+	{.name = "DesVarCapWt",
+	 .descr = "Desired Variable Capacity Weight"},
+	{.name = "DedDonMode",
+	 .descr = "Dedicated Donation Mode"},
+	{.name = "partition_entitled_capcity",
+	 .descr = "Partition Entitled Capacity"},
+	{.name = "system_active_processors",
+	 .descr = "Active Physical CPUs in system"},
+	{.name = "pool",
+	 .descr = "Shared Pool ID"},
+	{.name = "pool_capacity",
+	 .descr = "Maximum Capacity of Pool",
+	 .get = &get_percent_entry},
+	{.name = "pool_idle_time",
+	 .descr = "Shared Processor Pool Idle Time"},
+	{.name = "pool_num_procs",
+	 .descr = "Shared Processor Pool Processors"},
+	{.name = "unallocated_capacity_weight",
+	 .descr = "Unallocated Weight"},
+	{.name = "capacity_weight",
+	 .descr = "Entitled Capacity of Pool"},
+	{.name = "capped",
+	 .descr = "Mode",
+	 .get = &get_capped_mode},
+	{.name = "unallocated_capacity",
+	 .descr = "Unallocated Processor Capacity"},
+	{.name = "physical_procs_allocated_to_virtualization",
+	 .descr = "Physical Processor Allocated to Virtualization"},
+	{.name = "max_proc_entitled_capacity",
+	 .descr = "Maximum Processor Capacity Available to Pool"},
+	{.name = "entitled_proc_capacity_available",
+	 .descr = "Entitled Capacity of Pool"},
+	{.name = "dispatches",
+	 .descr = "Virtual Processor Dispatch Counter"},
+	{.name = "dispatch_dispersions",
+	 .descr = "Virtual Processor Dispersions"},
+	{.name = "purr",
+	 .descr = "Processor Utilization Resource Register"},
+	{.name = "partition_active_processors",
+	 .descr = "Online Virtual CPUs"},
+	{.name = "partition_potential_processors",
+	 .descr = "Maximum Virtual CPUs"},
+	{.name = "shared_processor_mode",
+	 .descr = "Type",
+	 .get = &get_smt_state},
+	{.name = "slb_size",
+	 .descr = "SLB Entries"},
+	{.name = "MinMem",
+	 .descr = "Minimum Memory"},
+	{.name = "DesMem",
+	 .descr = "Desired Memory"},
+	{.name = "entitled_memory",
+	 .descr = "Total I/O Memory Entitlement"},
+	{.name = "mapped_entitled_memory",
+	 .descr = "Total I/O Mapped Entitled Memory"},
+	{.name = "entitled_memory_group_number",
+	 .descr = "Memory Group ID of LPAR"},
+	{.name = "entitled_memory_pool_number",
+	 .descr = "Memory Pool ID"},
+	{.name = "entitled_memory_pool_size",
+	 .descr = "Physical Memory in the Pool"},
+	{.name = "entitled_memory_weight",
+	 .descr = "Variable Memory Capacity Weight"},
+	{.name = "unallocated_entitled_memory_weight",
+	 .descr = "Unallocated Variable Memory Capacity Weight"},
+	{.name = "unallocated_io_mapping_entitlement",
+	 .descr = "Unallocated I/O Memory Entitlement"},
+	{.name = "entitled_memory_loan_request",
+	 .descr = "Entitled Memory Loan Request"},
+	{.name = "backing_memory",
+	 .descr = "Backing Memory"},
+	{.name = "cmo_enabled",
+	 .descr = "Active Memory Sharing Enabled"},
+	{.name = "cmo_faults",
+	 .descr = "Active Memory Sharing Page Faults"},
+	{.name = "cmo_fault_time_usec",
+	 .descr = "Active Memory Sharing Fault Time"},
+	{.name = "cmo_primary_psp",
+	 .descr = "Primary VIOS Partition ID"},
+	{.name = "cmo_secondary_psp",
+	 .descr = "Secondary VIOS Partition ID"},
+	{.name = "cmo_page_size",
+	 .descr = "Physical Page Size"},
+
+	/* /proc/meminfo */
+	{.name = "MemTotal",
+	 .descr = "Online Memory",
+	 .get = &get_mem_total},
+
+	/* ppc64_cpu --smt */
+	{.name = "smt_state",
+	 .descr = "SMT",
+	 .get = &get_smt_state},
+
+	/* /proc/stat */
+	{.name = "cpu_total",
+	 .descr = "CPU Total Time"},
+	{.name = "cpu_user",
+	 .descr = "CPU User Time",
+	 .get = &get_cpu_stat},
+	{.name = "cpu_nice",
+	 .descr = "CPU Nice Time",
+	 .get = &get_cpu_stat},
+	{.name = "cpu_sys",
+	 .descr = "CPU System Time",
+	 .get = &get_cpu_stat},
+	{.name = "cpu_idle",
+	 .descr = "CPU Idle Time",
+	 .get = &get_cpu_stat},
+	{.name = "cpu_iowait",
+	 .descr = "CPU I/O Wait Time",
+	 .get = &get_cpu_stat},
+	{.name = "cpu_lbusy",
+	 .descr = "Logical CPU Utilization",
+	 .get = &get_cpu_stat},
+
+	/* placeholders for derived values */
+	{.name = "active_cpus_in_pool",
+	 .descr = "Active CPUs in Pool",
+	 .get = &get_active_cpus_in_pool},
+	{.name = "phys_cpu_percentage",
+	 .descr = "Physical CPU Percentage",
+	 .get = &get_phys_cpu_percentage},
+	{.name = "memory_mode",
+	 .descr = "Memory Mode",
+	 .get = &get_memory_mode},
+	{.name = "physc",
+	 .descr = "Physical CPU Consumed",
+	 .get = &get_cpu_physc},
+
+	/* Time */
+	{.name = "time",
+	 .descr = "Time"},
+
+	/* /proc/cpuinfo */
+	{.name = "timebase",
+	 .descr = "Timebase"},
+
+	{.name[0] = '\0'},
+};
+
+char *iflag_entries[] = {
+	"node_name",
+	"partition_name",
+	"partition_id",
+	"shared_processor_mode",
+	"capped",
+	"DesEntCap",
+	"group",
+	"pool",
+	"partition_active_processors",
+	"partition_potential_processors",
+	"MinProcs",
+	"MemTotal",
+	"MinMem",
+	"MaxMem",
+	"DesVarCapWt",
+	"MinEntCap",
+	"partition_max_entitled_capacity",
+	"CapInc",
+	"max_system_cpus",
+	"system_active_processors",
+	"active_cpus_in_pool",
+	"shared_cpus_in_system",
+	"pool_capacity",
+	"entitled_proc_capacity_available",
+	"unallocated_capacity",
+	"phys_cpu_percentage",
+	"unallocated_capacity_weight",
+	"memory_mode",
+	"entitled_memory",
+	"entitled_memory_weight",
+	"entitled_memory_pool_number",
+	"entitled_memory_pool_size",
+	"hypervisor_page_size",
+	"unallocated_entitled_memory_weight",	
+	"unallocated_io_mapping_entitlement",
+	"entitled_memory_group_number",
+	"desired_virt_cpus",
+	"desired_memory",
+	"DesVarCapWt",
+	"desired_capacity",
+	"target_mem_factor",
+	"target_mem_size",
+	NULL
+};
+
