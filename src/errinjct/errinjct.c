70c9d16ea08762db2976ab4597da203c198da28d
Nathan Fontenot <nfont@linux.vnet.ibm.com>
2016-01-08 10:44:19 -0500

    Move the powerpc-utils package from the IBM license to the GNU GPL license.
    Signed-off-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>

--- src/errinjct/errinjct.c
+++ src/errinjct/errinjct.c
@@ -8,7 +8,20 @@
  * It uses librtas to inject artificial errors into the system.
  *
  * Copyright (c) 2004 IBM Corporation
- * Common Public License Version 1.0 (see COPYRIGHT)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 #define _GNU_SOURCE
