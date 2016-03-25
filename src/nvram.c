ibm-power-utilities/powerpc-utils

commit b15b0a7f9ffcd1bc099a166723475bb53bc945cf

nvram: don't allow long names
The maximum partition name length is 12 characters but nvram accepts
strings of any length. This change will enforce the maximum length
passed to options that take a partition name to avoid confusion.

Signed-off-by: Joel Stanley <joel@jms.id.au>

src/nvram.c
@@ -45,6 +45,7 @@
 #include <inttypes.h>
 #include <zlib.h>
 #include <endian.h>
+#include <stdbool.h>
 
 #include "nvram.h"
 
@@ -452,6 +453,16 @@ nvram_parse_partitions(struct nvram *nvram)
     return 0;
 }
 
+bool part_name_valid(const char *name)
+{
+    if (strlen(name) > MAX_PART_NAME) {
+        err_msg("partition name maximum length is %d\n", MAX_PART_NAME);
+        return false;
+    }
+
+    return true;
+}
+
 /**
  * nvram_find_fd_partition
  * @brief Find a particular nvram partition using a file descriptor
@@ -466,6 +477,9 @@ nvram_find_fd_partition(struct nvram *nvram, char *name)
     struct partition_header	phead;
     int				len;
     int				found = 0;
+
+    if (part_name_valid(name))
+	    return -1;
 
     if (lseek(nvram->fd, SEEK_SET, 0) == -1) {
         err_msg("could not seek to beginning of file %s\n", nvram->filename);
@@ -1461,12 +1475,18 @@ main (int argc, char *argv[])
 		break;
 	    case 'd':	/* dump */
 		dump_name = optarg;
+                if (!part_name_valid(dump_name))
+                    exit(1);
 		break;
 	    case 'a':	/* ASCII dump */
 	    	ascii_name = optarg;
+                if (!part_name_valid(ascii_name))
+                    exit(1);
 		break;
 	    case 'z':	/* dump compressed data */
-		zip_name = optarg;
+                zip_name = optarg;
+                if (!part_name_valid(zip_name))
+                    exit(1);
 		break;
 	    case 'n':	/* nvram-file */
 		nvram.filename = optarg;
@@ -1509,6 +1529,8 @@ main (int argc, char *argv[])
 		break;
 	    case 'p':	/* update-config partition name */
 	        config_pname = optarg;
+                if (!part_name_valid(config_pname))
+                    exit(1);
 		break;
 	    case '?':
 		exit(1);

src/nvram.h
@@ -45,14 +45,20 @@
 #define MAX_CPUS 128
 
 /**
+ * @def MAX_PART_NAME
+ * @brief maximum number of bytes in partition name
+ */
+#define MAX_PART_NAME 12
+
+/**
  * @struct partition_header
  * @brief nvram partition header data
  */
 struct partition_header {
-    unsigned char	signature;  /**< partition signature */
-    unsigned char 	checksum;   /**< partition checksum */
-    unsigned short 	length;     /**< partition length */
-    char 		name[12];   /**< partition name */
+    unsigned char	signature;		/**< partition signature */
+    unsigned char 	checksum;		/**< partition checksum */
+    unsigned short 	length;			/**< partition length */
+    char 		name[MAX_PART_NAME];	/**< partition name */
 };
 
 /* sub-header for error-log partitions */

commit bd7e19a9ff5aebe741b0520906660dac1ecf4a2a

nvram: Correct valid partition name checking
A recent update to not allow long names had a bug in that it did
not correctly check the return value when verifying a partition name.

Fixes: b15b0a7f9ffc (nvram: don't allow long names)
Signed-off-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>
CC: Joel Stanley <joel@jms.id.au>

src/nvram.c
@@ -491,7 +491,7 @@ nvram_find_fd_partition(struct nvram *nvram, char *name)
     int				len;
     int				found = 0;
 
-    if (part_name_valid(name))
+    if (!part_name_valid(name))
 	    return -1;
 
     if (lseek(nvram->fd, SEEK_SET, 0) == -1) {