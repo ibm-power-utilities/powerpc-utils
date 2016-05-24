9fc079cf39d999040483a00d3b41000ca3e43013
John Allen <jallen@linux.vnet.ibm.com>
2016-05-24 16:28:27 -0400

    drmgr: Workaround for concurrent PRRN collisions
    Proposed workaround for issues with current PRRN event handling mechanism.
    
    Currently, the mechanism works by kicking off a script that calls a drmgr
    remove followed by a drmgr add for each effected device. With this method,
    problems can occur when multiple prrn events arrive in a short time span or
    when other dlpar operations are happening while the script is running. What
    can happen is that these other dlpar operations can jump in inbetween when
    the script calls the remove and the add and take the drmgr lock. Then when
    we try to add the memory back from the script, we are unable to take the
    drmgr lock and the add fails.
    
    This patch fixes this issue by creating a new -P flag for drmgr which will
    perform the necessary removes/adds from within the drmgr lock. The new
    option will be invoked as follows:
    
    drmgr -P <path to prrn log file>
    
    On the rtas_errd side, we will begin by creating a log file for the
    PRRN event which will contain a list of the type and the drc index for
    each effected device. Then instead of invoking the script, we will call
    the above drmgr command with the path to the prrn log file. Then, on the
    drmgr side, we parse the list and perform the necessary removes/adds and
    then delete the prrn log file.
    
    Signed-off-by: John Allen <jallen@linux.vnet.ibm.com>

---
+++ src/drmgr/prrn.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "dr.h"
+#include "drmem.h"
+#include "drcpu.h"
+
+int handle_prrn(char *filename)
+{
+	struct options opts;
+	char fmt_drc[11];
+	char type[4];
+	char drc[9];
+	int rc = 0;
+	FILE *fd;
+
+	fd = fopen(filename, "r");
+	if (!fd) {
+		say(ERROR, "Failed to open the file %s\n", filename);
+		return -1;
+	}
+
+	set_output_level(4);
+
+	memset(&opts, 0, sizeof(opts));
+
+	while (fscanf(fd, "%3s %8s\n", type, drc) == 2) {
+		/* Set up options struct */
+		opts.ctype = type;
+		sprintf(fmt_drc, "0x%s", drc);
+		opts.usr_drc_name = fmt_drc;
+
+		set_timeout(PRRN_TIMEOUT);
+
+		if (!strcmp(type, "mem")) {
+			opts.action = REMOVE;
+			rc = drslot_chrp_mem(&opts);
+			if (rc)
+				continue;
+
+			opts.action = ADD;
+			drslot_chrp_mem(&opts);
+		} else if (!strcmp(type, "cpu")) {
+			opts.action = REMOVE;
+			rc = drslot_chrp_cpu(&opts);
+			if (rc)
+				continue;
+
+			opts.action = ADD;
+			drslot_chrp_cpu(&opts);
+		} else {
+			say(ERROR, "Device type \"%s\" not recognized.\n",
+			    type);
+			continue;
+		}
+	}
+
+	return 0;
+}
