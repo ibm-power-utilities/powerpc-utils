a97c5664f53f9a18dd0b941a842d7884253cf8ed
Nathan Fontenot <nfont@linux.vnet.ibm.com>
2016-07-11 17:06:26 -0400

    drmgr: Disable use of in-kernel cpu hotplug
    After submitting the patch to update drmgr to use the in-kernel hotplug
    capabilities for memory and cpu, commit <bbc5fc07c54d> (drmgr: Start using
    in-kernel DLPAR functionality for cpu/memory), I realized there
    are still distro kernels that do not have the in-kernel cpu hotplug
    capabilities. Using the latest drmgr with this update breaks cpu dlpar
    because the cpu dlpar code assumes that if the dlpar sysfs file exists
    then in-kernel cpu dlpar is possible. this is wrong, the sysfs dlpar file
    was added with in-kernel memory hotplug, its existence can only guarantee
    memory hotplug capability.
    
    For now, disable using the in-kernel cpu capabilities by drmgr until
    this can be resolved.
    
    Signed-off-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>

--- src/drmgr/common_cpu.c
+++ src/drmgr/common_cpu.c
@@ -657,6 +657,7 @@ acquire_cpu(struct dr_node *cpu, struct dr_info *dr_info)
 	return 0;
 }
 
+#ifdef NOT_YET
 int do_cpu_kernel_dlpar(struct dr_node *cpu, int action)
 {
 	char cmdbuf[256];
@@ -680,6 +681,7 @@ int do_cpu_kernel_dlpar(struct dr_node *cpu, int action)
 
 	return do_kernel_dlpar(cmdbuf, offset);
 }
+#endif
 
 int
 probe_cpu(struct dr_node *cpu, struct dr_info *dr_info)
@@ -689,9 +691,11 @@ probe_cpu(struct dr_node *cpu, struct dr_info *dr_info)
 	int write_len;
 	int rc = 0;
 
+#ifdef NOT_YET
 	if (kernel_dlpar_exists()) {
 		rc = do_cpu_kernel_dlpar(cpu, ADD);
 	} else {
+#endif
 		probe_file = open(CPU_PROBE_FILE, O_WRONLY);
 		if (probe_file <= 0) {
 			/* Attempt to add cpu from user-space, this may be
@@ -727,7 +731,9 @@ probe_cpu(struct dr_node *cpu, struct dr_info *dr_info)
 
 			close(probe_file);
 		}
+#ifdef NOT_YET
 	}
+#endif
 
 	if (!rc) {
 		update_cpu_node(cpu, NULL, dr_info);
@@ -791,8 +797,10 @@ release_cpu(struct dr_node *cpu, struct dr_info *dr_info)
 	int release_file;
 	int rc;
 
+#ifdef NOT_YET
 	if (kernel_dlpar_exists())
 		return do_cpu_kernel_dlpar(cpu, REMOVE);
+#endif
 
 	release_file = open(CPU_RELEASE_FILE, O_WRONLY);
 	if (release_file > 0) {
