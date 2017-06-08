ibm-power-utilities/powerpc-utils

commit f9986ac430113c96d0398efb44159c22410bdf5f

drmgr: Move options.ctype to global usr_drc_type
Move the ctype member of struct options out to be the global usr_drc_type.
This also makes the usr_drc_type an enum drc_type.

The type change to the enum allows for checking the type directly
instead of the numerous strcmp() operations that are done.

Signed-off-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>

src/drmgr/common.c
@@ -113,14 +113,14 @@ static int check_kmods(struct options *opts)
 	int rc;
 
 	/* We only need to do this for PHB/SLOT/PCI operations */
-	if (opts->ctype && strcmp(opts->ctype, "pci")
-	    && strcmp(opts->ctype, "phb") && strcmp(opts->ctype, "slot"))
+	if (usr_drc_type == DRC_TYPE_PCI || usr_drc_type == DRC_TYPE_PHB ||
+	    usr_drc_type == DRC_TYPE_SLOT)
 		return 0;
 
 	/* We don't use rpadlar_io/rpaphp for PCI operations run with the
 	 * -v / virtio flag, which relies on generic PCI rescan instead
 	 */
-	if (opts->ctype && !strcmp(opts->ctype, "pci") && opts->pci_virtio == 1)
+	if (usr_drc_type == DRC_TYPE_PCI && opts->pci_virtio == 1)
 		return 0;
 
 	/* Before checking for dlpar capability, we need to ensure that
@@ -1166,7 +1166,7 @@ update_sysparm(struct options *opts)
 	char *linux_parm = NULL;
 	
 	/* Validate capability */
-	if (! strcmp(opts->ctype, "cpu")) {
+	if (usr_drc_type == DRC_TYPE_CPU) {
 		if (! cpu_entitlement_capable()) {
 			say(ERROR, "CPU entitlement capability is not enabled "
 			    "on this platform.\n");
@@ -1174,7 +1174,7 @@ update_sysparm(struct options *opts)
 		}
 
 		sysparm_table = cpu_sysparm_table;
-	} else if (! strcmp(opts->ctype, "mem")) {
+	} else if (usr_drc_type == DRC_TYPE_MEM) {
 		if (! mem_entitlement_capable()) {
 			say(ERROR, "Memory entitlement capability is not "
 			    "enabled on this platform.\n");
@@ -1183,8 +1183,8 @@ update_sysparm(struct options *opts)
 
 		sysparm_table = mem_sysparm_table;
 	} else {
-		say(ERROR, "Invalid entitlement update type \"%s\" "
-		    "specified.\n", opts->ctype);
+		say(ERROR, "Invalid entitlement update type \"%d\" "
+		    "specified.\n", usr_drc_type);
 		return -1;
 	}
 	
@@ -1480,3 +1480,33 @@ int do_kernel_dlpar(const char *cmd, int cmdlen)
 	say(INFO, "Success\n");
 	return 0;
 }
+
+enum drc_type to_drc_type(const char *arg)
+{
+	if (!strncmp(arg, "pci", 3))
+		return DRC_TYPE_PCI;
+
+	if (!strncmp(arg, "slot", 4))
+		return DRC_TYPE_SLOT;
+
+	if (!strncmp(arg, "phb", 3))
+		return DRC_TYPE_PHB;
+
+	if (!strncmp(arg, "cpu", 3))
+		return DRC_TYPE_CPU;
+
+	if (!strncmp(arg, "mem", 3))
+		return DRC_TYPE_MEM;
+
+	if (!strncmp(arg, "port", 4))
+		return DRC_TYPE_PORT;
+
+	if (!strncmp(arg, "phib", 4))
+		return DRC_TYPE_HIBERNATE;
+
+	if (!strncmp(arg, "pmig", 4))
+		return DRC_TYPE_MIGRATION;
+
+	return DRC_TYPE_NONE;
+}
+

src/drmgr/dr.h
@@ -58,7 +58,6 @@ void * __zalloc(size_t, const char *, int);
 #define DRMIG_COMMAND		"drmig_chrp_%s"
 
 struct options {
-	char	*ctype;
 	char	*p_option;
 	int     pci_virtio;     /* qemu virtio device (legacy guest workaround) */
 	char    *prrn_filename;
@@ -85,6 +84,11 @@ struct options {
 /* Global User Specifications */
 enum drmgr_action {NONE, ADD, REMOVE, QUERY, REPLACE, IDENTIFY,
 		   MIGRATE, HIBERNATE};
+
+enum drc_type {DRC_TYPE_NONE, DRC_TYPE_PCI, DRC_TYPE_SLOT, DRC_TYPE_PHB,
+	       DRC_TYPE_CPU, DRC_TYPE_MEM, DRC_TYPE_PORT,
+	       DRC_TYPE_HIBERNATE, DRC_TYPE_MIGRATION};
+
 extern enum drmgr_action usr_action;
 extern int usr_slot_identification;
 extern int usr_timeout;
@@ -92,6 +96,7 @@ extern char *usr_drc_name;
 extern uint32_t usr_drc_index;
 extern int usr_prompt;
 extern int usr_drc_count;
+extern enum drc_type usr_drc_type;
 
 enum say_level { ERROR = 1, WARN, INFO, DEBUG, EXTRA_DEBUG};
 
@@ -170,6 +175,8 @@ void phib_usage(char **);
 int ams_balloon_active(void);
 
 int is_display_adapter(struct dr_node *);
+
+enum drc_type to_drc_type(const char *);
 
 #define PRRN_TIMEOUT 30
 int handle_prrn(char *filename);

src/drmgr/drmgr.c
@@ -145,7 +145,7 @@ int
 valid_drmgr_options(struct options *opts)
 {
 
-	if (opts->ctype == NULL) {
+	if (usr_drc_type == DRC_TYPE_NONE) {
 		say(ERROR, "A connector type (-c) must be specified\n");
 		return -1;
 	}
@@ -195,7 +195,7 @@ parse_options(int argc, char *argv[], struct options *opts)
 			action_cnt++;
 			break;
 		    case 'c':
-			opts->ctype = optarg;
+			usr_drc_type = to_drc_type(optarg);
 			break;
 		    case 'C':
 			display_capabilities = 1;
@@ -282,38 +282,43 @@ get_command(struct options *opts)
 	if (usr_action == MIGRATE)
 		return &commands[DRMIG_CHRP_PMIG];
 
-	if (!opts->ctype)
-		return &commands[DRMGR];
-
-	if ((! strncmp(opts->ctype, "port", 4)) ||
-	    (usr_drc_name && !strncmp(usr_drc_name, "HEA", 3)))
+	if (usr_drc_name && !strncmp(usr_drc_name, "HEA", 3))
 		return &commands[DRSLOT_CHRP_HEA];
 	
-	if (! strcmp(opts->ctype, "slot"))
-		return &commands[DRSLOT_CHRP_SLOT];
-			
-	if (! strcmp(opts->ctype, "phb"))
+	switch (usr_drc_type) {
+	case DRC_TYPE_NONE:
+		return &commands[DRMGR];
+		break;
+	case DRC_TYPE_PORT:
+ 		return &commands[DRSLOT_CHRP_HEA];
+		break;
+	case DRC_TYPE_SLOT:
+ 		return &commands[DRSLOT_CHRP_SLOT];
+		break;
+	case DRC_TYPE_PHB:
 		return &commands[DRSLOT_CHRP_PHB];
-			
-	if (! strcmp(opts->ctype, "pci"))
-		return &commands[DRSLOT_CHRP_PCI];
-			
-	if (! strcmp(opts->ctype, "mem"))
-		return &commands[DRSLOT_CHRP_MEM];
-			
-	if (! strcmp(opts->ctype, "cpu"))
-		return &commands[DRSLOT_CHRP_CPU];
-
-	if (! strcmp(opts->ctype, "phib")) {
-		usr_action = HIBERNATE;
-		return &commands[DRSLOT_CHRP_PHIB];
+		break;
+	case DRC_TYPE_PCI:
+ 		return &commands[DRSLOT_CHRP_PCI];
+		break;
+	case DRC_TYPE_MEM:
+ 		return &commands[DRSLOT_CHRP_MEM];
+		break;
+	case DRC_TYPE_CPU:
+ 		return &commands[DRSLOT_CHRP_CPU];
+		break;
+	case DRC_TYPE_HIBERNATE:
+ 		usr_action = HIBERNATE;
+ 		return &commands[DRSLOT_CHRP_PHIB];
+		break;
+	default:
+		/* If we make it this far, the user specified an invalid
+		 * connector type.
+		 */
+		say(ERROR, "Dynamic reconfiguration is not supported for "
+		    "connector\ntype \"%s\" on this system\n", usr_drc_type);
+		break;
 	}
-			
-	/* If we make it this far, the user specified an invalid
-	 * connector type.
-	 */
-	say(ERROR, "Dynamic reconfiguration is not supported for connector\n"
-	    "type \"%s\" on this system\n", opts->ctype);
 
 	return &commands[DRMGR];
 }

src/drmgr/drmig_chrp_pmig.c
@@ -550,7 +550,7 @@ valid_pmig_options(struct options *opts)
 	}
 
 	/* Determine if this is a migration or a hibernation request */
-	if (!strcmp(opts->ctype, "pmig")) {
+	if (usr_drc_type == DRC_TYPE_MIGRATION) {
 		if (usr_action != MIGRATE) {
 			/* The -m option must be specified with migrations */
 			say(ERROR, "The -m must be specified for migrations\n");
@@ -561,7 +561,7 @@ valid_pmig_options(struct options *opts)
 			say(ERROR, "Partition Mobility is not supported.\n");
 			return -1;
 		}
-	} else if (!strcmp(opts->ctype, "phib")) {
+	} else if (usr_drc_type == DRC_TYPE_HIBERNATE) {
 		if (!phib_capable()) {
 			say(ERROR, "Partition Hibernation is not supported.\n");
 			return -1;
@@ -569,8 +569,8 @@ valid_pmig_options(struct options *opts)
 
 		usr_action = HIBERNATE;
 	} else {
-		say(ERROR, "The value \"%s\" for the -c option is not valid\n",
-		    opts->ctype);
+		say(ERROR, "The value \"%d\" for the -c option is not valid\n",
+		    usr_drc_type);
 		return -1;
 	}
 

src/drmgr/drslot_chrp_hea.c
@@ -360,13 +360,13 @@ drslot_chrp_hea(struct options *opts)
 		break;
 
 	    case REMOVE:
-		if (! strcmp(opts->ctype, "port"))
+		if (usr_drc_type == DRC_TYPE_PORT)
 			rc = remove_port(opts);
-		else if (! strcmp(opts->ctype, "slot"))
+		else if (usr_drc_type == DRC_TYPE_SLOT)
 			rc = remove_hea(opts);
 		else {
-			say(ERROR, "The connector type %s is not supported.\n",
-			    opts->ctype);
+			say(ERROR, "The connector type %d is not supported.\n",
+			    usr_drc_type);
 			rc = -1;
 		}
 		break;

src/drmgr/options.c
@@ -42,3 +42,6 @@ int usr_prompt = 1;
 
 /* user specified number of devices to add/remove */
 int usr_drc_count = 0;
+
+/* user specified drc type to use */
+enum drc_type usr_drc_type = DRC_TYPE_NONE;

src/drmgr/prrn.c
@@ -26,7 +26,7 @@ int handle_prrn(char *filename)
 
 	while (fscanf(fd, "%3s %8s\n", type, drc) == 2) {
 		/* Set up options struct */
-		opts.ctype = type;
+		usr_drc_type = to_drc_type(type);
 		sprintf(fmt_drc, "0x%s", drc);
 		usr_drc_name = fmt_drc;
 

commit 479f1a1122f7d9025d3a21e60a0431d2eba6ee2f

drmgr: Correct drc type checking in check_kmods()
Fixes: f9986ac43011 ("drmgr: Move options.ctype to global usr_drc_type")

When the conversion to using the global usr_drc_type was
made the logic checking for whic drc types we need to check
the rpadlpar_io kernbel module for was broken. This corrects
the check.

Signed-off-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>
Signed-off-by: Tyrel Datwyler <tyreld@linux.vnet.ibm.com>

src/drmgr/common.c
@@ -113,8 +113,8 @@ static int check_kmods(void)
 	int rc;
 
 	/* We only need to do this for PHB/SLOT/PCI operations */
-	if (usr_drc_type == DRC_TYPE_PCI || usr_drc_type == DRC_TYPE_PHB ||
-	    usr_drc_type == DRC_TYPE_SLOT)
+	if (usr_drc_type != DRC_TYPE_PCI && usr_drc_type != DRC_TYPE_PHB &&
+	    usr_drc_type != DRC_TYPE_SLOT)
 		return 0;
 
 	/* We don't use rpadlar_io/rpaphp for PCI operations run with the