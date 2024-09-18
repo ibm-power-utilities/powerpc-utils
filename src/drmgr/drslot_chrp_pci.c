ibm-power-utilities/powerpc-utils

commit 4e6670df0da2b92a33dd880ce987823358f298bf

drmgr/pci: Add multipath partner device support for hotplug add
If the PCI device has multipath partner device, the firmware
provides partner DRC index in "ibm,multipath-partner-drc"
property and this property is available in the PCI device node.
So when the PCI device add is initiated, both paths will be
added if the partner path is also configured and enabled with
the following steps:

- Identify slot and notify user to add the device
- Add the path and enable for the specified device
- Find the partner path DRC index from "ibm,multipath-partner-drc"
  property for the specified device
- Add the partner path if it is also configured
- Notify user about adding partner path

Since both paths will be using the same slot, LED indicators and
the slot identification will be done only for the primary device.

Signed-off-by: Haren Myneni <haren@linux.ibm.com>
Signed-off-by: Tyrel Datwyler <tyreld@linux.ibm.com>

src/drmgr/drslot_chrp_pci.c
@@ -31,6 +31,7 @@
 #include "rtas_calls.h"
 #include "dr.h"
 #include "drpci.h"
+#include "ofdt.h"
 
 static char *sw_error = "Internal software error. Contact your service "
 			"representative.\n";
@@ -367,27 +368,38 @@ static int do_identify(struct dr_node *all_nodes)
  * off, isolated, and the LED is turned off.
  *
  * @param slot
+ * @param partner path or not
  * @returns 0 on success, !0 on failure
  */
-static int add_work(struct dr_node *node)
+static int add_work(struct dr_node *node, bool partner_device)
 {
-	int pow_state;	/* Tells us if power was turned on when	 */
-	int iso_state;	/* Tells us isolation state after 	 */
+	int pow_state = POWER_OFF;	/* Tells us if power was turned */
+					/* on when	 */
+	int iso_state = ISOLATE;	/* Tells us isolation state after */
 	int rc;
 	struct of_node *new_nodes;/* nodes returned from configure_connector */
 
-	/* if we're continuing, set LED_ON and see if a card is really there. */
-	if (process_led(node, LED_ON))
-		return -1;
+	/*
+	 * Already checked the card presence for the original device
+	 * and both multipaths use the same card. So do not need to
+	 * check the card presence again for the partner device.
+	 */
+	if (!partner_device) {
+		/* if we're continuing, set LED_ON and see if a card */
+		/* is really there. */
+		if (process_led(node, LED_ON))
+			return -1;
 
-	say(DEBUG, "is calling card_present\n");
-	rc = card_present(node, &pow_state, &iso_state);
-	if (!rc) {
-		say(ERROR, "No PCI card was detected in the specified "
-		    "PCI slot.\n");
-		rtas_set_indicator(ISOLATION_STATE, node->drc_index, ISOLATE);
-		set_power(node->drc_power, POWER_OFF);
-		return -1;
+		say(DEBUG, "is calling card_present\n");
+		rc = card_present(node, &pow_state, &iso_state);
+		if (!rc) {
+			say(ERROR, "No PCI card was detected in the specified "
+					"PCI slot.\n");
+			rtas_set_indicator(ISOLATION_STATE, node->drc_index,
+					ISOLATE);
+			set_power(node->drc_power, POWER_OFF);
+			return -1;
+		}
 	}
 
 	if (!pow_state) {
@@ -461,7 +473,7 @@ static int add_work(struct dr_node *node)
  * power to a slot off. The prompts the user to insert the new card
  * into the slot.
  */
-static int do_insert_card_work(struct dr_node *node)
+static int do_insert_card_work(struct dr_node *node, bool partner_device)
 {
 	int rc;
 
@@ -495,18 +507,18 @@ static int do_insert_card_work(struct dr_node *node)
 		return -1;
 	}
 
-	if (usr_prompt) {
+	if (usr_prompt && !partner_device) {
 		/* Prompt user to put in card and to press
 		 * Enter to continue or other key to exit.
 		 */
 		if (process_led(node, LED_ACTION))
 			return -1;
 
-		printf("The visual indicator for the specified PCI slot has\n"
-			"been set to the action state. Insert the PCI card\n"
-			"into the identified slot, connect any devices to be\n"
-			"configured and press Enter to continue. Enter x to "
-			"exit.\n");
+		printf("The visual indicator for the PCI slot <%s>\n"
+			"has been set to the action state. Insert the PCI\n"
+			"card into the identified slot, connect any devices\n"
+			"to be configured and press Enter to continue.\n"
+			"Enter x to exit.\n", node->drc_name);
 
 		if (!(getchar() == '\n')) {
 			process_led(node, LED_OFF);
@@ -543,6 +555,58 @@ find_partner_node(struct dr_node *node, struct dr_node *all_nodes)
 	return partner_node;
 }
 
+static int insert_add_work(struct dr_node *node, bool partner_device)
+{
+	int usr_key = USER_CONT;
+	int rc;
+
+	if (!partner_device) {
+		/* Prompt user only if in interactive mode. */
+		if (usr_prompt) {
+			if (usr_slot_identification)
+				usr_key = identify_slot(node);
+
+			if (usr_key == USER_QUIT) {
+				if (node->children == NULL)
+					process_led(node, LED_OFF);
+				else
+					process_led(node, LED_ON);
+				return 0;
+			}
+		}
+
+		if (node->children != NULL) {
+			/* If there's already something here, turn the
+			 * LED on and exit with user error.
+			 */
+			process_led(node, LED_ON);
+			say(ERROR, "The specified PCI slot is already occupied.\n");
+			return -1;
+		}
+	}
+
+	if (!pci_hotplug_only) {
+		rc = do_insert_card_work(node, partner_device);
+		if (rc)
+			return rc;
+	}
+
+	/* Call the routine which determines
+	 * what the user wants and does it.
+	*/
+        rc = add_work(node, partner_device);
+        if (rc)
+                return rc;
+
+	/*
+	 * Need to populate w/ children to retrieve partner device
+	 */
+	if (init_node(node))
+		return -1;
+
+	return 1;
+}
+
 /**
  * do_add
  *
@@ -560,8 +624,7 @@ find_partner_node(struct dr_node *node, struct dr_node *all_nodes)
  */
 static int do_add(struct dr_node *all_nodes)
 {
-	struct dr_node *node;
-	int usr_key = USER_CONT;
+	struct dr_node *node, *partner_node = NULL;
 	int rc;
 
 	node = find_slot(usr_drc_name, 0, all_nodes, 0);
@@ -573,41 +636,19 @@ static int do_add(struct dr_node *all_nodes)
 		return -1;
 	}
 
-	/* Prompt user only if in interactive mode. */
-	if (usr_prompt) {
-		if (usr_slot_identification)
-			usr_key = identify_slot(node);
+	rc = insert_add_work(node, false);
+	if (rc <= 0)
+		return rc;
 
-		if (usr_key == USER_QUIT) {
-			if (node->children == NULL)
-				process_led(node, LED_OFF);
-			else
-				process_led(node, LED_ON);
-			return 0;
-		}
-	}
-
-	if (node->children != NULL) {
-		/* If there's already something here, turn the
-		 * LED on and exit with user error.
-		 */
-		process_led(node, LED_ON);
-		say(ERROR, "The specified PCI slot is already occupied.\n");
-		return -1;
-	}
-
-	if (!pci_hotplug_only) {
-		rc = do_insert_card_work(node);
-		if (rc)
+	partner_node = find_partner_node(node, all_nodes);
+	if (partner_node) {
+		printf("<%s> and <%s> are\nmultipath partner devices. "
+			"So <%s> is\nalso added.\n", node->drc_name,
+			partner_node->drc_name, partner_node->drc_name);
+		rc = insert_add_work(partner_node, true);
+		if (rc <= 0)
 			return rc;
 	}
-
-	/* Call the routine which determines
-	 * what the user wants and does it.
-	 */
-	rc = add_work(node);
-	if (rc)
-		return rc;
 
 	say(DEBUG, "is calling enable_slot to config adapter\n");
 
@@ -615,9 +656,12 @@ static int do_add(struct dr_node *all_nodes)
 	 * qemu pci slots so we let the generic kernel pci code probe the device
 	 * by rescanning the bus in the qemu virtio case.
 	 */
-	if (!pci_virtio)
+	if (!pci_virtio) {
 		set_hp_adapter_status(PHP_CONFIG_ADAPTER, node->drc_name);
-	else
+		if (partner_node)
+			set_hp_adapter_status(PHP_CONFIG_ADAPTER,
+					partner_node->drc_name);
+	} else
 		pci_rescan_bus();
 
 	return 0;
@@ -896,7 +940,7 @@ static int do_replace(struct dr_node *all_nodes)
 		}
 	}
 
-	rc = add_work(repl_node);
+	rc = add_work(repl_node, false);
 	if (rc)
 		return rc;
 
@@ -917,7 +961,7 @@ static int do_replace(struct dr_node *all_nodes)
 		if (remove_work(repl_node, false))
 			return -1;
 
-		rc = add_work(repl_node);
+		rc = add_work(repl_node, false);
 		if (!rc)
 			set_hp_adapter_status(PHP_CONFIG_ADAPTER,
 					      repl_node->drc_name);

src/drmgr/common_pci.c
@@ -295,7 +295,7 @@ add_child_node(struct dr_node *parent, char *child_path)
  * @param node
  * @returns 0 on success, !0 otherwise
  */
-static int
+int
 init_node(struct dr_node *node)
 {
 	DIR *d;

src/drmgr/ofdt.h
@@ -184,6 +184,7 @@ int get_min_common_depth(void);
 int get_assoc_arrays(const char *dir, struct assoc_arrays *aa,
 		     int min_common_depth);
 int of_associativity_to_node(const char *dir, int min_common_depth);
+int init_node(struct dr_node *);
 
 static inline int aa_index_to_node(struct assoc_arrays *aa, uint32_t aa_index)
 {

commit 06c9922a774a6c096acbc69664646386c9e185b5

drmgr/pci: Do not add partner device if exists in the device tree
For PCI hotplug interface, the partner device will be added or
removed if configured with the primary device add / remove.
Whereas for PHB interface, drmgr notifies the user to add / remove
partner device if configured with the primary device. So there is
a possibility of partner PCI node exists in the device tree when
PCI interface for ADD is executed. The current ADD code in
drslot_chrp_pci.c does not check whether the partner device node
is present and may add / enable partner device again which may give
EEH errors.

See the following sequence to get this scenario:
drmgr -r -c phb -s "PHB 1336"   --> Remove primary PHB node
drmgr -r -c pci -s "U50EE.001.WZS000E-P3-C24-R2" --> Remove
partner PCI node
drmgr -a -c phb -s "PHB 1336"  --> Add primary PHB node
drmgr -a -c pci -s "U50EE.001.WZS000E-P3-C24-R2"  --> Add partner
PCI node and can try to add primary PCI node again. In this case
"U50EE.001.WZS000E-P3-C24-R1".

This patch fixes the issue by checking the device node in the
device tree and add the device if does not present and remove
only if the corresponding device node exists.

Fixes: 4e6670df0da2b92 ("drmgr/pci: Add multipath partner device support for hotplug add")
Signed-off-by: Haren Myneni <haren@linux.ibm.com>
[tyreld: fixed up white space and replace/remove phrasing]
Signed-off-by: Tyrel Datwyler <tyreld@linux.ibm.com>

src/drmgr/drslot_chrp_pci.c
@@ -536,11 +536,14 @@ static int do_insert_card_work(struct dr_node *node, bool partner_device)
  * Find the partner DRC index and retrieve the partner node.
  */
 static struct dr_node *
-find_partner_node(struct dr_node *node, struct dr_node *all_nodes)
+find_partner_node(struct dr_node *node, struct dr_node *all_nodes,
+		int *dt_entry_present)
 {
 	struct dr_node *partner_node = NULL;
 	uint32_t partner_index = 0;
 	int rc;
+
+	*dt_entry_present = 0;
 
 	/*
 	 * Expect the partner device only for the PCI node
@@ -550,8 +553,20 @@ find_partner_node(struct dr_node *node, struct dr_node *all_nodes)
 
 	/* Find the multipath partner device index if available */
 	rc = get_my_partner_drc_index(node->children, &partner_index);
-	if (!rc)
+	if (!rc) {
 		partner_node = find_slot(NULL, partner_index, all_nodes, 1);
+		/*
+		 * Do not add if the partner entry is already present.
+		 * Nothing to remove if the partner node does not exists.
+		 */
+		if (partner_node && partner_node->children) {
+			uint32_t index;
+			rc = get_my_drc_index(partner_node->children->ofdt_path,
+					&index);
+			if (!rc && (partner_index == index))
+				*dt_entry_present = 1;
+		}
+	}
 
 	return partner_node;
 }
@@ -626,6 +641,7 @@ static int insert_add_work(struct dr_node *node, bool partner_device)
 static int do_add(struct dr_node *all_nodes)
 {
 	struct dr_node *node, *partner_node = NULL;
+	int dt_entry;
 	int rc;
 
 	node = find_slot(usr_drc_name, 0, all_nodes, 0);
@@ -641,8 +657,8 @@ static int do_add(struct dr_node *all_nodes)
 	if (rc <= 0)
 		return rc;
 
-	partner_node = find_partner_node(node, all_nodes);
-	if (partner_node) {
+	partner_node = find_partner_node(node, all_nodes, &dt_entry);
+	if (partner_node && !dt_entry) {
 		printf("<%s> and <%s> are\nmultipath partner devices. "
 			"So <%s> is\nalso added.\n", node->drc_name,
 			partner_node->drc_name, partner_node->drc_name);
@@ -839,16 +855,25 @@ static int remove_work(struct dr_node *node, bool partner_device)
 static int do_remove(struct dr_node *all_nodes)
 {
 	struct dr_node *node, *partner_node = NULL;
+	int dt_entry;
 
 	node = find_slot(usr_drc_name, 0, all_nodes, 0);
 	if (node == NULL)
 		return -1;
 
-	partner_node = find_partner_node(node, all_nodes);
-        if (partner_node)
-		printf("<%s> and <%s> are\nmultipath partner devices. "
-			"So <%s> will\nbe also removed.\n", node->drc_name,
-			partner_node->drc_name, partner_node->drc_name);
+	partner_node = find_partner_node(node, all_nodes, &dt_entry);
+	if (partner_node) {
+		/*
+		 * Remove partner device if exists in the device tree.
+		 */
+		if (dt_entry)
+			printf("<%s> and <%s> are\nmultipath partner devices. "
+				"So <%s> will\nalso be removed.\n",
+				node->drc_name, partner_node->drc_name,
+				partner_node->drc_name);
+		else
+			partner_node = NULL;
+	}
 
 	/* Remove the specified slot and update the device-tree */
 	if (remove_work(node, false))
@@ -939,18 +964,23 @@ static int replace_add_work(struct dr_node *node, bool partner_device)
 static int do_replace(struct dr_node *all_nodes)
 {
 	struct dr_node *repl_node, *partner_node = NULL;
+	int dt_entry;
 	int rc;
 
 	repl_node = find_slot(usr_drc_name, 0, all_nodes, 0);
 	if (repl_node == NULL)
 		return -1;
 
-	partner_node = find_partner_node(repl_node, all_nodes);
-	if (partner_node)
-		printf("<%s> and <%s> are\nmultipath partner devices. "
-			"So <%s> will\nbe also replaced.\n",
-			repl_node->drc_name, partner_node->drc_name,
-			partner_node->drc_name);
+	partner_node = find_partner_node(repl_node, all_nodes, &dt_entry);
+	if (partner_node) {
+		if (dt_entry)
+			printf("<%s> and <%s> are\nmultipath partner devices. "
+				"So <%s> will\nalso be replaced.\n",
+				repl_node->drc_name, partner_node->drc_name,
+				partner_node->drc_name);
+		else
+			partner_node = NULL;
+	}
 
 	/* Call the routine which does the work of getting the node info,
 	 * then removing it from the OF device tree.
@@ -972,9 +1002,11 @@ static int do_replace(struct dr_node *all_nodes)
 	if (rc <= 0)
 		return rc;
 
-	rc = replace_add_work(partner_node, true);
-	if (rc <= 0)
-		return rc;
+	if (partner_node) {
+		rc = replace_add_work(partner_node, true);
+		if (rc <= 0)
+			return rc;
+	}
 
 	if (repl_node->post_replace_processing) {
 		int prompt_save = usr_prompt;
@@ -988,8 +1020,8 @@ static int do_replace(struct dr_node *all_nodes)
 		if (remove_work(repl_node, false))
 			return -1;
 
-		partner_node = find_partner_node(repl_node, node);
-		if (partner_node) {
+		partner_node = find_partner_node(repl_node, node, &dt_entry);
+		if (partner_node && dt_entry) {
 			if (remove_work(partner_node, true))
 				return -1;
 		}