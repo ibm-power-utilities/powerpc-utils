ibm-power-utilities/powerpc-utils

commit efa87d93231eb0fe4f406bf58e7d5aa783ee4f13

lparstat: Correct presentation of some stats
lparstat.c: Currently there are several problems with the presentation
of statistics by lparstat's default output mode.  These issues include:
 * Showing negative values for certain fields e.g. %user, %sys, %wait,
   %lbusy.
 * Showing zero values for certain fields e.g. physc, %entc
 * Identifying systems with 'Shared' or 'Dedicated' Memory Mode
 * Calculating the number if logical CPUs used by a system
This patch looks to correct these issues.  It also changes the widths
of the physc, %entc fields which had too little precision available
to show the value change on some systems.

Signed-off-by: Michael Bringmann <mwb@linux.vnet.ibm.com>

src/lparstat.c
@@ -134,7 +134,7 @@ void get_cpu_physc(struct sysentry *unused_se, char *buf)
 	old_purr = strtoll(se->old_value, NULL, 0);
 
 	physc = (new_purr - old_purr)/timebase/elapsed;
-	sprintf(buf, "%.2f", physc);
+	sprintf(buf, "%.6f", physc);
 }
 
 void get_per_entc(struct sysentry *unused_se, char *buf)
@@ -146,7 +146,7 @@ void get_per_entc(struct sysentry *unused_se, char *buf)
 	get_sysdata("DesEntCap", &descr, entc);
 	get_sysdata("physc", &descr, physc);
 
-	sprintf(buf, "%.2f", atof(physc) / atof(entc));
+	sprintf(buf, "%.6f", atof(physc) / atof(entc));
 }
 
 int parse_lparcfg()
@@ -318,16 +318,21 @@ void get_active_cpus_in_pool(struct sysentry *se, char *buf)
 {
 	struct sysentry *tmp;
 
-	tmp = get_sysentry("pool_capacity");
-	sprintf(buf, "%d", atoi(tmp->value)/100);
+	tmp = get_sysentry("physical_procs_allocated_to_virtualization");
+	if (tmp) {
+		sprintf(buf, "%d", atoi(tmp->value));
+	} else {
+		tmp = get_sysentry("pool_capacity");
+		sprintf(buf, "%d", atoi(tmp->value)/100);
+	}
 }
 
 void get_memory_mode(struct sysentry *se, char *buf)
 {
 	struct sysentry *tmp;
 
-	tmp = get_sysentry("entitled_memory");
-	if (tmp->value[0] == '\0')
+	tmp = get_sysentry("entitled_memory_pool_number");
+	if (atoi(tmp->value) == 65535)
 		sprintf(buf, "Dedicated");
 	else
 		sprintf(buf, "Shared");
@@ -485,7 +490,7 @@ int print_iflag_data()
 
 void print_default_output(int interval, int count)
 {
-	char *fmt = "%5s %5s %5s %5s %5s %5s %5s %5s %5s\n";
+	char *fmt = "%5s %5s %5s %8s %8s %5s %5s %5s %5s\n";
 	char *descr;
 	char buf[128];
 	int offset;

commit 26c9baef1e15d5e8a9a9310f057ca18b3123db81

lparstat: Use pool_capacity for determining active cpus in a pool
The function get_active_cpus_in_pool() primarily used the value from the
physical_procs_allocated_to_virtualization sysentry to represent the
cpus active in a pool. This is incorrect as that value represents
the physical processors in the system allocated to virtualization.

This patch changes the get_active_cpus_in_pool() function to only use
the pool_capacity sysentry for correctly determining the active cpus
in a pool.

Fixes: efa87d93231e ("lparstat: Correct presentation of some stats")

Signed-off-by: Luciano Chavez <chavez@us.ibm.com>
Signed-off-by: Tyrel Datwyler <tyreld@linux.ibm.com>

src/lparstat.c
@@ -785,13 +785,8 @@ void get_active_cpus_in_pool(struct sysentry *se, char *buf)
 {
 	struct sysentry *tmp;
 
-	tmp = get_sysentry("physical_procs_allocated_to_virtualization");
-	if (tmp) {
-		sprintf(buf, "%d", atoi(tmp->value));
-	} else {
-		tmp = get_sysentry("pool_capacity");
-		sprintf(buf, "%d", atoi(tmp->value)/100);
-	}
+	tmp = get_sysentry("pool_capacity");
+	sprintf(buf, "%d", atoi(tmp->value)/100);
 }
 
 void get_memory_mode(struct sysentry *se, char *buf)